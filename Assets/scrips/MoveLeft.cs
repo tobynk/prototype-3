facc9e98aaa4bf671fe23d9ac51aa9d9b4d68694
tobynk <122053994+tobynk@users.noreply.github.com>
2023-02-02 12:41:55 -0800

    on 6.Stop obstacle spawning on gameOver

--- Assets/scrips/MoveLeft.cs
+++ Assets/scrips/MoveLeft.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed=30;
+    private PlayerController PlayerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControllerScript= GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left* Time.deltaTime*speed);
+        if (PlayerControllerScript.gameOver==false){
+            transform.Translate(Vector3.left* Time.deltaTime*speed);
+        }
+        
     }
 }
--- Assets/scrips/PlayerController.cs
+++ Assets/scrips/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce=10;
     public float gravityModifier;
     public bool isOnGround=true;
+    public bool gameOver= false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +23,16 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
             isOnGround=false;
         }
+        
     }
 
     private void OnCollisionEnter(Collision collision){
-        isOnGround=true;
+        if (collision.gameObject.CompareTag("ground")){
+            isOnGround=true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")){
+            gameOver=true;
+            Debug.Log("game over");
+        }
     }
 }
