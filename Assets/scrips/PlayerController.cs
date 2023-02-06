f1bbb9d0976e87b0a3d1a1be8bb5aafd0fe136b7
tobynk <122053994+tobynk@users.noreply.github.com>
2023-02-06 11:59:15 -0800

    done wit Lesson 3.4 - Particles and Sound Effects

--- Assets/scrips/PlayerController.cs
+++ Assets/scrips/PlayerController.cs
@@ -10,12 +10,19 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround=true;
     public bool gameOver= false;
     private Animator playerAnim; 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+     private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         playerRb= GetComponent<Rigidbody>();
         Physics.gravity*=gravityModifier;
         playerAnim = GetComponent<Animator>();
+        dirtParticle.Stop();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,8 +30,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)&& isOnGround&& !gameOver){
             playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            dirtParticle.Stop();
             isOnGround=false;
              playerAnim.SetTrigger("Jump_trig");
+             playerAudio.PlayOneShot(jumpSound, 1.0f);
+             
         }
         
     }
@@ -32,6 +42,7 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision){
         if (collision.gameObject.CompareTag("ground")){
             isOnGround=true;
+            dirtParticle.Play();
             
         }
         else if (collision.gameObject.CompareTag("Obstacle")){
@@ -39,6 +50,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("game over");
             playerAnim.SetBool("Death_b",true);
             playerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
 
         }
     }
